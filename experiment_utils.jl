7dd1c7e440cc3637b30fc5f5d74db7807e418de8
lboyles <leviboyles@gmail.com>
2013-02-05 16:21:06 -0800

    parallel fixes and symmetric_split option

--- experiment_utils.jl
+++ experiment_utils.jl
@@ -1,5 +1,5 @@
-require("mcmc.jl")
-require("data_utils.jl")
+@everywhere require("mcmc.jl")
+@everywhere require("data_utils.jl")
 
 function train_test_split(Y::Array{Int64, 2},
                           train_pct::Float64,
@@ -39,74 +39,7 @@ function train_test_split(Y::Array{Int64, 2},
     (Ytrain, Ytest)
 end
 
-function run_batch(model_spec::ModelSpecification,
-                   Y::Array{Int64,2},
-                   train_pct::Float64,
-                   lambda::Float64,
-                   gamma::Float64,
-                   num_iterations::Int,
-                   burnin_iterations::Int,
-                   num_trials::Int,
-                   id_string,
-                   result_path)
-
-    assert(burnin_iterations < num_iterations)
-
-    X_r = zeros((0,0,0))
-    X_p = zeros((0,0))
-    X_c = zeros((0,0))
-
-    datas = Array(Any, num_trials) 
-    for i = 1:num_trials
-        Ytrain, Ytest = train_test_split(Y, train_pct, model_spec.symmetric_W)
-        datas[i] = DataState(Ytrain, Ytest, copy(X_r), copy(X_p), copy(X_c))
-    end
-
-    if num_trials > 1
-        result_paths = [copy(result_path) for i = 1:num_trials]
-        id_strings = [copy(id_string) for i = 1:num_trials]
-        trials = linspace(1,num_trials,num_trials)
-
-        lambdas = lambda*ones(num_trials)
-        gammas = gamma*ones(num_trials)
-        model_specs = [copy(model_spec) for i = 1:num_trials]
-        num_iterses = num_iterations*ones(Int, num_trials)
-        burn_iterses = burnin_iterations*ones(Int, num_trials)
-
-        pmap(run_and_save, result_paths, id_strings, trials, datas, 
-             lambdas, gammas, model_specs, num_iterses, burn_iterses)
-
-    else
-        run_and_save(result_path, id_string, 1, datas[1], 
-             lambda, gamma, model_spec, num_iterations, burnin_iterations)
-    end
-
-#    run_and_save(result_paths[1], id_strings[1], train_pcts[1], trials[1], datas[1],
-#         lambdas[1], gammas[1], model_specs[1], num_iterses[1], burn_iterses[1])
-
-#    for trial = 1:num_trials
-#        result_refs[trial] = @spawn mcmc(data, lambda, gamma, model_spec,
-#                                         num_iterations, burnin_iterations)
-#        println(result_refs[trial]) 
-#    end
-
-#    @parallel for trial = 1:num_trials
-#        run_and_save(result_path, id_string, train_pct, trial,
-#                   datas[trial], lambda, gamma, model_spec, num_iterations, burnin_iterations)
-#    end
-#    for trial = 1:num_trials
-#        trial_string = "$(id_string)_$(train_pct)_$(trial)"
-#        results[trial] = fetch(result_refs[trial])
-#        println(results[trial][1:end-1])
-#        models = results[trial][end]
-#        model = models[end]
-#        save("$result_path/metrics_$trial_string.jla", results[trial][1:end-1])
-#        save("$result_path/model_$trial_string.jla", model2array(model))
-#    end
-
-end
-
-function run_and_save(result_path, id_string, trial, mcmc_args...)
+@everywhere function run_and_save(result_path, id_string, trial, mcmc_args...)
     results = mcmc(mcmc_args...)
     trial_string = "$(id_string)_$(trial)"
     models = results[end]
@@ -124,3 +57,84 @@ function run_and_save(result_path, id_string, trial, mcmc_args...)
     save("$result_path/weights_$trial_string.jla", weights_array)
     save("$result_path/data_$trial_string.jla", datas)
 end
+
+function run_batch(model_spec::ModelSpecification,
+                   Y::Array{Int64,2},
+                   symmetric_split::Bool,
+                   train_pct::Float64,
+                   lambda::Float64,
+                   gamma::Float64,
+                   num_iterations::Int,
+                   burnin_iterations::Int,
+                   num_trials::Int,
+                   id_string,
+                   result_path)
+
+    assert(burnin_iterations < num_iterations)
+
+    X_r = zeros((0,0,0))
+    X_p = zeros((0,0))
+    X_c = zeros((0,0))
+
+    datas = Array(Any, num_trials) 
+    for i = 1:num_trials
+        Ytrain, Ytest = train_test_split(Y, train_pct, symmetric_split)
+        datas[i] = DataState(Ytrain, Ytest, copy(X_r), copy(X_p), copy(X_c))
+    end
+
+    if num_trials > 1
+        result_paths = [copy(result_path) for i = 1:num_trials]
+        id_strings = [copy(id_string) for i = 1:num_trials]
+        trials = linspace(1,num_trials,num_trials)
+
+        lambdas = lambda*ones(num_trials)
+        gammas = gamma*ones(num_trials)
+        model_specs = [copy(model_spec) for i = 1:num_trials]
+        num_iterses = num_iterations*ones(Int, num_trials)
+        burn_iterses = burnin_iterations*ones(Int, num_trials)
+
+        pmap(run_and_save, result_paths, id_strings, trials, datas, 
+             lambdas, gammas, model_specs, num_iterses, burn_iterses)
+
+#        remote_refs = Array(Any, num_trials)
+#
+#        for trial = 1:num_trials
+#            remote_refs[trial] = @spawn run_and_save(result_path, id_string, trial, datas[trial], lambda, 
+#                         gamma, model_spec, num_iterations, burnin_iterations)
+#            println(remote_refs[trial])
+#        end
+#
+#        for trial = 1:num_trials
+#            wait(remote_refs[trial])
+#        end
+
+#        @parallel for trial = 1:num_trials
+#            run_and_save(result_path, id_string, trial, datas[trial], lambda, 
+#                         gamma, model_spec, num_iterations, burnin_iterations)
+#        end
+    else
+        run_and_save(result_path, id_string, 1, datas[1], 
+             lambda, gamma, model_spec, num_iterations, burnin_iterations)
+    end
+
+#    run_and_save(result_paths[1], id_strings[1], train_pcts[1], trials[1], datas[1],
+#         lambdas[1], gammas[1], model_specs[1], num_iterses[1], burn_iterses[1])
+
+#    for trial = 1:num_trials
+#        result_refs[trial] = @spawn mcmc(data, lambda, gamma, model_spec,
+#                                         num_iterations, burnin_iterations)
+#        println(result_refs[trial]) 
+#    end
+
+#    for trial = 1:num_trials
+#        trial_string = "$(id_string)_$(train_pct)_$(trial)"
+#        results[trial] = fetch(result_refs[trial])
+#        println(results[trial][1:end-1])
+#        models = results[trial][end]
+#        model = models[end]
+#        save("$result_path/metrics_$trial_string.jla", results[trial][1:end-1])
+#        save("$result_path/model_$trial_string.jla", model2array(model))
+#    end
+
+end
+
--- run_nips_coauthorship_experiments.jl
+++ run_nips_coauthorship_experiments.jl
@@ -1,17 +1,18 @@
-@everywhere require("mcmc.jl")
-@everywhere require("data_utils.jl")
+@everywhere require("model.jl")
 @everywhere require("read_nips_data.jl")
-@everywhere require("experiment_utils.jl")
+require("experiment_utils.jl")
 
-symmetric = true
-model_spec = ModelSpecification(false, false, false, symmetric, false, ones(3)/3, 0.2, 1.0, false, false)
+model_spec = ModelSpecification(false, false, false, false, false, ones(3)/3, 0.2, 1.0, false, false)
+
 
 X_r = zeros((0,0,0))
 X_p = zeros((0,0))
 X_c = zeros((0,0))
 
+symmetric_split = true
 trnpct = 0.8
-run_batch(model_spec, YY, trnpct, 0.1, 0.5, 500, 200, 10, "NIPS_test_$trnpct", "../results")
+run_batch(model_spec, YY, symmetric_split, trnpct, 0.1, 0.5, 500, 200, 10, 
+          "NIPS_test_$trnpct", "../results")
 
 # split into train/test
 #Ytrain, Ytest = train_test_split(YY, .8)
